a9d343a648ee344c4fea58ba0b9f52535f6f3437
am0xil <spark_wang@yeah.net>
2020-12-12 14:02:07 +0800

    【blog】首次提交项目

---
+++ src/main/java/cn/amoxil/zeroblog/ZeroblogApplication.java
@@ -0,0 +1,13 @@
+package cn.amoxil.zeroblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/amoxil/zeroblog/ZeroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.amoxil.zeroblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
